3c8d1a02cde8d7df9583eaf8e2518df3b324f59e
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-10 20:47:03 -0600

    added buttons for navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+
+//popper for dropdown button
+import ElementPopper from "react-element-popper";
 
 function Nav() {
+  const [active, setActive] = useState(false);
+  const toggleDropDown = () => setActive(!active);
 
-return (
-//need links to:
-    //about me
-    //projects
-    //contact
-);
-};
+  //Resume button
+  const resumeButton = (
+    //click changes the state - set active to true
+    <button onClick={toggleDropDown}>Resume</button>
+  );
+
+  //Resume dropdown option
+  const option = (
+    <ul className="dropdown">
+      <li href="#">View or Download Resume</li>
+    </ul>
+  );
+
+  return (
+    <nav class="navbar">
+      <button class="btn" type="button" href="">
+        About Me
+      </button>
+      <button class="btn" type="button" href="">
+        Projects
+      </button>
+      <button class="btn" type="button" href="">
+        Projects
+      </button>
+      <ElementPopper
+        element={resumeButton}
+        popper={active && option}
+        position="bottom-left"
+      />
+    </nav>
+  );
+}
 
 export default Nav;
