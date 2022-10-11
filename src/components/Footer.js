dcb83e19371fdf01d144982cd833ecdb33aea508
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-10 21:51:34 -0600

    added social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
+  return (
+    <footer>
+      <a
+        class="btn btn-primary"
+        href="https://github.com/alverson98"
+        role="button"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+      <a
+        class="btn btn-primary"
+        href="https://www.linkedin.com/in/hannah-alverson-5a7892183"
+        role="button"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        class="btn btn-primary"
+        href="https://www.google.com/"
+        role="button"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Stack Overflow
+      </a>
+    </footer>
+  );
+}
 
-    return (
-    //github link
-    //linkedIn link
-    //third social link - make account for stack overflow
-    );
-    };
-    
-    export default Footer;
+export default Footer;
