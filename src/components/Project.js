68814850b136c07d8ca90153e67a94514c0db903
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-12 13:41:22 -0600

    added basic structure for project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,90 @@
 import React from 'react';
 
 function Project() {
-
-    return (
+  return (
+    <div class="project-section">
+      <h2>Projects</h2>
+      <div class="project-cards">
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="" alt="" />
+          <div class="card-body">
+            <h5 class="card-title">Project Name Here</h5>
+            <a href="#" class="btn btn-primary">
+              GitHub Repository
+            </a>
+            <a href="#" class="btn btn-primary">
+              Deployed Application
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
     //6 projects as cards - bootstrap
     //screenshots of each project
     //need links to deployed app
     //need links to github repos
-    );
-    };
-    
-    export default Project;
+  );
+}
+
+export default Project;
+
