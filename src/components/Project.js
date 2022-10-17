6a99b729b7fd0858ba9c90e752e25d068d563f36
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-17 11:06:38 -0600

    added repo and deployed links for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,78 +1,138 @@
-import React from 'react';
+import React from "react";
 
 function Project() {
   return (
-    <div class="project-section">
+    <div className="project-section">
       <h2>Projects</h2>
-      <div class="project-cards">
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+      <div className="project-cards">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Trip $plit</h5>
+            <a
+              href="https://github.com/alverson98/trip_split"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://trip-split-atoz.herokuapp.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Middle-Where</h5>
+            <a
+              href="https://github.com/alverson98/middleWhere"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://limitless-temple-38712.herokuapp.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">FiHub</h5>
+            <a
+              href="https://github.com/alverson98/FiHub"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://alverson98.github.io/FiHub/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Weather Dashboard</h5>
+            <a
+              href="https://github.com/alverson98/weather-forecast-data"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://alverson98.github.io/weather-forecast-data/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Work Schedule</h5>
+            <a
+              href="https://github.com/alverson98/work-schedule"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://alverson98.github.io/work-schedule/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src="" alt="" />
-          <div class="card-body">
-            <h5 class="card-title">Project Name Here</h5>
-            <a href="#" class="btn btn-primary">
+        <div className="card">
+          <img className="card-img-top" src="" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Coding Quiz</h5>
+            <a
+              href="https://github.com/alverson98/code-quiz"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               GitHub Repository
             </a>
-            <a href="#" class="btn btn-primary">
+            <a
+              href="https://alverson98.github.io/code-quiz/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
               Deployed Application
             </a>
           </div>
@@ -87,4 +147,3 @@ function Project() {
 }
 
 export default Project;
-
